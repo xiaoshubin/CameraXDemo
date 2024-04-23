6490dd2606184db4ab6af40dc531061c5b1740f1
xiaoshubin <495303648@qq.com>
2024-04-23 15:53:15 +0800

    二维码扫描优化

--- app/src/main/java/com/example/cameraxdemo/QrCodeActivity.kt
+++ app/src/main/java/com/example/cameraxdemo/QrCodeActivity.kt
@@ -1,9 +1,9 @@
 package com.example.cameraxdemo
 
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.support.annotation.Nullable
-import android.text.TextUtils
 import android.util.Log
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
@@ -12,6 +12,7 @@ import androidx.camera.view.CameraController.COORDINATE_SYSTEM_VIEW_REFERENCED
 import androidx.camera.view.LifecycleCameraController
 import androidx.camera.view.PreviewView
 import androidx.core.content.ContextCompat
+import androidx.core.content.FileProvider
 import com.example.cameraxdemo.databinding.ActivityQrCodeBinding
 import com.google.mlkit.vision.barcode.BarcodeScanner
 import com.google.mlkit.vision.barcode.BarcodeScannerOptions
@@ -20,8 +21,10 @@ import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
 import com.hjq.permissions.Permission
 import com.hjq.permissions.XXPermissions
+import java.io.File
 import java.util.concurrent.ExecutorService
 import java.util.concurrent.Executors
+
 
 /**
  * 二维码扫描
@@ -37,6 +40,10 @@ import java.util.concurrent.Executors
  * 缺点:
  * 1.汉字64个的二维码可以快速扫描,64以上的就很慢,100以上基本扫描不出来
  *
+ *
+ * 注意:
+ * 1. 7.0以上需要以FileProvider的方式获取Uri
+ * 2. 目前无法通过BarcodeScanning解析图片
  */
 class QrCodeActivity : AppCompatActivity() {
     private lateinit var bind:ActivityQrCodeBinding
@@ -58,6 +65,25 @@ class QrCodeActivity : AppCompatActivity() {
                 }
             }
 
+        //从相册选择
+        bind.ivPhoto.setOnClickListener {
+            XXPermissions.with(this)
+                .permission(
+                    Permission.READ_MEDIA_IMAGES
+                    ,Permission.READ_MEDIA_VIDEO
+                    ,Permission.READ_MEDIA_AUDIO
+                )
+                .request { _, all ->
+                    if (all){
+                        selectPic()
+                    }else{
+                        Toast.makeText(this,"未获取相册存储权限", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+                }
+
+        }
+
     }
 
     /**
@@ -78,10 +104,7 @@ class QrCodeActivity : AppCompatActivity() {
      * 数据矩阵 (FORMAT_DATA_MATRIX)
      */
     private fun startCamera() {
-        //从相册选择
-        bind.ivPhoto.setOnClickListener {
-            selectPic()
-        }
+
         var cameraController = LifecycleCameraController(baseContext)
         val previewView: PreviewView = bind.viewFinder
 
@@ -90,7 +113,8 @@ class QrCodeActivity : AppCompatActivity() {
             .build()
         barcodeScanner = BarcodeScanning.getClient(options)
         cameraController.setImageAnalysisAnalyzer(cameraExecutor,
-                MlKitAnalyzer(listOf(barcodeScanner), COORDINATE_SYSTEM_VIEW_REFERENCED,ContextCompat.getMainExecutor(this@QrCodeActivity)) { result: MlKitAnalyzer.Result? ->
+                MlKitAnalyzer(listOf(barcodeScanner), COORDINATE_SYSTEM_VIEW_REFERENCED,
+                    ContextCompat.getMainExecutor(this@QrCodeActivity)) { result: MlKitAnalyzer.Result? ->
                 val barcodeResults = result?.getValue(barcodeScanner)
                 //没有数据返回,继续扫描
                 if ((barcodeResults == null) || (barcodeResults.size == 0) || (barcodeResults.first() == null)) {
@@ -131,30 +155,46 @@ class QrCodeActivity : AppCompatActivity() {
         if (requestCode == PICK_IMAGE_REQUEST&&resultcode == RESULT_OK &&data!=null){
             val uri = data.data
             if (uri!=null){
-                //识别图片二维码
-                val inputImage = InputImage.fromFilePath(this@QrCodeActivity,uri)
-                barcodeScanner.process(inputImage).addOnSuccessListener { barcodeResults ->
-                        //没有数据返回,继续扫描
-                        val previewView: PreviewView = bind.viewFinder
-                        if ((barcodeResults == null) || (barcodeResults.size == 0) || (barcodeResults.first() == null)) {
-                            previewView.overlay.clear()
-                            previewView.setOnTouchListener { _, _ -> false } //no-op
-                            return@addOnSuccessListener
-                        }
-                        val str = barcodeResults[0].rawValue.toString()
-                        Log.i(TAG,"扫描出来的二维码数据:$str")
-                        Toast.makeText(this,"$str", Toast.LENGTH_SHORT).show()
-                        //在这里关闭线程池,关闭扫描,避免弹出多个扫描结果
-                        cameraExecutor.shutdown()
-                        barcodeScanner.close()
-                        finish()
+                val realPath = getPathFromUri(uri)
+                val uriForFile = FileProvider.getUriForFile(this,"$packageName.fileprovider", File(realPath))
 
-                    }
-                    .addOnFailureListener {
-                        Toast.makeText(this,"解析失败", Toast.LENGTH_SHORT).show()
-                    }
+                bind.ivPhoto.setImageURI(uriForFile)
+                //识别图片二维码
+                val inputImage = InputImage.fromFilePath(this@QrCodeActivity,uriForFile)
+                scanBarcodes(inputImage)
+
             }
         }
+    }
+
+    private fun scanBarcodes(image: InputImage) {
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_QR_CODE)
+            .build()
+        val scanner = BarcodeScanning.getClient(options)
+        scanner.process(image)
+            .addOnSuccessListener { barcodes ->
+                if ((barcodes == null) || (barcodes.size == 0) || (barcodes.first() == null)) {
+                    Log.i(TAG,"未解析到任何数据")
+                    return@addOnSuccessListener
+                }
+                val str = barcodes[0].rawValue.toString()
+                Log.i(TAG,"扫描出来的二维码数据:$str")
+            }
+            .addOnFailureListener {
+                Log.i(TAG,"解析失败")
+            }
+    }
+
+
+    private fun getPathFromUri(uri: Uri):String{
+        val filePathcolumn = arrayOf(android.provider.MediaStore.Images.Media.DATA)
+        val cursor = contentResolver.query(uri, filePathcolumn, null, null, null)
+        cursor?.moveToFirst()
+        val columnIndex =cursor?.getColumnIndex(filePathcolumn[0])?:0
+        val imagePath =cursor?.getString(columnIndex)
+        cursor?.close()
+        return imagePath?:""
     }
     companion object {
         private const val TAG = "CameraX-MLKit"
@@ -162,3 +202,4 @@ class QrCodeActivity : AppCompatActivity() {
     }
 
 }
+
